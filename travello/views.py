478942a33850113f57b5fcd2af9ff5bb6cf21732
passedonwisdom <yash.deorah@somaiya.edu>
2021-04-16 20:59:37 +0530

    first

---
+++ travello/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Destination
+# Create your views here.
+
+def index(request):
+    dest = Destination.objects.all()
+
+    return render(request,"index.html",{'dests': dest})
